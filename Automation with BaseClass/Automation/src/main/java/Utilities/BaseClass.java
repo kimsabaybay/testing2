68e733fc192225f9b8a47d0b4fbf85348d065b44
Kim Sabaybay <ksabaybay@belusallc.com>
2024-02-12 11:23:06 -0500

    headless mode for browsers and dependencies update

--- Automation with BaseClass/Automation/src/main/java/Utilities/BaseClass.java
+++ Automation with BaseClass/Automation/src/main/java/Utilities/BaseClass.java
@@ -11,8 +11,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -34,12 +37,27 @@ public class BaseClass {
     public void setUp(Method method,@Optional("Chrome") String browser) {
         if (browser.equalsIgnoreCase("Chrome")) {
             WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-dev-shm-usage");
             driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("Firefox")) {
             WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-dev-shm-usage");
             driver = new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("Edge")) {
             WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-dev-shm-usage");
             driver = new EdgeDriver();
         }else {
             throw new IllegalArgumentException("Broser not supported" +browser);
